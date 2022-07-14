444021f633f3e0fc50ccf52c162674ec80331ff7
fossephate <matt.cfosse@gmail.com>
2022-07-14 18:37:40 -0400

    tablet mode fix

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1399,7 +1399,7 @@ class _AppHomePageState extends State<AppHomePage> with WidgetsBindingObserver, 
       // handle block handoff:
     } else if (result is AuthItem) {
       // handle auth handoff:
-      
+
     }
   }
 
@@ -1603,14 +1603,6 @@ class _AppHomePageState extends State<AppHomePage> with WidgetsBindingObserver, 
         resizeToAvoidBottomInset: false,
         key: _scaffoldKey,
         backgroundColor: StateContainer.of(context).curTheme.background,
-        // drawerEdgeDragWidth: 180,
-        // drawerScrimColor: StateContainer.of(context).curTheme.barrierWeaker,
-        // drawer: SizedBox(
-        //   width: UIUtil.tabletDrawerWidth(context),
-        //   child: Drawer(
-        //     child: SettingsSheet(),
-        //   ),
-        // ),
         body: SafeArea(
           minimum: EdgeInsets.only(
             top: MediaQuery.of(context).size.height * 0.045, /*bottom: MediaQuery.of(context).size.height * 0.035*/
@@ -1627,123 +1619,128 @@ class _AppHomePageState extends State<AppHomePage> with WidgetsBindingObserver, 
                 width: UIUtil.getDrawerAwareScreenWidth(context),
                 height: MediaQuery.of(context).size.height,
                 margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.height * 0.015),
-                child: Expanded(
-                  child: Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: <Widget>[
-                      //Everything else
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Stack(
+                        alignment: Alignment.bottomCenter,
                         children: <Widget>[
-                          //Main Card
-                          _buildMainCard(context, _scaffoldKey),
-                          //Main Card End
-                          //Transactions Text
-                          Container(
-                            margin: const EdgeInsetsDirectional.fromSTEB(30.0, 20.0, 26.0, 0.0),
-                            child: Row(
-                              children: <Widget>[
-                                Text(
-                                  CaseChange.toUpperCase(AppLocalization.of(context)!.transactions, context),
-                                  textAlign: TextAlign.start,
-                                  style: TextStyle(
-                                    fontSize: 14.0,
-                                    fontWeight: FontWeight.w100,
-                                    color: StateContainer.of(context).curTheme.text,
-                                  ),
+                          //Everything else
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              //Main Card
+                              _buildMainCard(context, _scaffoldKey),
+                              //Main Card End
+                              //Transactions Text
+                              Container(
+                                margin: const EdgeInsetsDirectional.fromSTEB(30.0, 20.0, 26.0, 0.0),
+                                child: Row(
+                                  children: <Widget>[
+                                    Text(
+                                      CaseChange.toUpperCase(AppLocalization.of(context)!.transactions, context),
+                                      textAlign: TextAlign.start,
+                                      style: TextStyle(
+                                        fontSize: 14.0,
+                                        fontWeight: FontWeight.w100,
+                                        color: StateContainer.of(context).curTheme.text,
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ],
-                            ),
-                          ), //Transactions Text End
-                          //Transactions List
-                          Expanded(
-                            child: Stack(
-                              children: <Widget>[
-                                // _getUnifiedListWidget(context),
-                                _getUnifiedListWidget(context),
-                                //List Top Gradient End
-                                Align(
-                                  alignment: Alignment.topCenter,
-                                  child: Container(
-                                    height: 10.0,
-                                    width: double.infinity,
-                                    decoration: BoxDecoration(
-                                      gradient: LinearGradient(
-                                        colors: [StateContainer.of(context).curTheme.background00!, StateContainer.of(context).curTheme.background!],
-                                        begin: const AlignmentDirectional(0.5, 1.0),
-                                        end: const AlignmentDirectional(0.5, -1.0),
+                              ), //Transactions Text End
+                              //Transactions List
+                              Expanded(
+                                child: Stack(
+                                  children: <Widget>[
+                                    // _getUnifiedListWidget(context),
+                                    _getUnifiedListWidget(context),
+                                    //List Top Gradient End
+                                    Align(
+                                      alignment: Alignment.topCenter,
+                                      child: Container(
+                                        height: 10.0,
+                                        width: double.infinity,
+                                        decoration: BoxDecoration(
+                                          gradient: LinearGradient(
+                                            colors: [StateContainer.of(context).curTheme.background00!, StateContainer.of(context).curTheme.background!],
+                                            begin: const AlignmentDirectional(0.5, 1.0),
+                                            end: const AlignmentDirectional(0.5, -1.0),
+                                          ),
+                                        ),
                                       ),
-                                    ),
-                                  ),
-                                ), // List Top Gradient End
-                                //List Bottom Gradient
-                                Align(
-                                  alignment: Alignment.bottomCenter,
-                                  child: Container(
-                                    height: 30.0,
-                                    width: double.infinity,
-                                    decoration: BoxDecoration(
-                                      gradient: LinearGradient(
-                                        colors: [StateContainer.of(context).curTheme.background00!, StateContainer.of(context).curTheme.background!],
-                                        begin: const AlignmentDirectional(0.5, -1),
-                                        end: const AlignmentDirectional(0.5, 0.5),
+                                    ), // List Top Gradient End
+                                    //List Bottom Gradient
+                                    Align(
+                                      alignment: Alignment.bottomCenter,
+                                      child: Container(
+                                        height: 30.0,
+                                        width: double.infinity,
+                                        decoration: BoxDecoration(
+                                          gradient: LinearGradient(
+                                            colors: [StateContainer.of(context).curTheme.background00!, StateContainer.of(context).curTheme.background!],
+                                            begin: const AlignmentDirectional(0.5, -1),
+                                            end: const AlignmentDirectional(0.5, 0.5),
+                                          ),
+                                        ),
                                       ),
-                                    ),
-                                  ),
-                                ), //List Bottom Gradient End
-                              ],
-                            ),
-                          ), //Transactions List End
-                          //Buttons background
-                          SizedBox(
-                            height: 55,
-                            width: MediaQuery.of(context).size.width,
-                          ), //Buttons background
-                        ],
-                      ),
-
-                      // Send / Receive Buttons
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: <Widget>[
-                          Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              boxShadow: [StateContainer.of(context).curTheme.boxShadowButton!],
-                            ),
-                            height: 55,
-                            width: (UIUtil.getDrawerAwareScreenWidth(context) - 42).abs() / 2,
-                            margin: const EdgeInsetsDirectional.only(start: 14, top: 0.0, end: 7.0),
-                            // margin: EdgeInsetsDirectional.only(start: 7.0, top: 0.0, end: 7.0),
-                            child: TextButton(
-                              key: const Key("home_receive_button"),
-                              style: TextButton.styleFrom(
-                                backgroundColor: receive != null ? StateContainer.of(context).curTheme.primary : StateContainer.of(context).curTheme.primary60,
-                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
-                                primary: receive != null ? StateContainer.of(context).curTheme.background40 : Colors.transparent,
-                                // highlightColor: receive != null ? StateContainer.of(context).curTheme.background40 : Colors.transparent,
-                                // splashColor: receive != null ? StateContainer.of(context).curTheme.background40 : Colors.transparent,
-                              ),
-                              child: AutoSizeText(
-                                AppLocalization.of(context)!.receive,
-                                textAlign: TextAlign.center,
-                                style: AppStyles.textStyleButtonPrimary(context),
-                                maxLines: 1,
-                                stepGranularity: 0.5,
-                              ),
-                              onPressed: () {
-                                if (receive == null) {
-                                  return;
-                                }
-                                Sheets.showAppHeightNineSheet(context: context, widget: receive!);
-                              },
-                            ),
+                                    ), //List Bottom Gradient End
+                                  ],
+                                ),
+                              ), //Transactions List End
+                              //Buttons background
+                              SizedBox(
+                                height: 55,
+                                width: MediaQuery.of(context).size.width,
+                              ), //Buttons background
+                            ],
                           ),
-                          AppPopupButton(),
+
+                          // Send / Receive Buttons
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: <Widget>[
+                              Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  boxShadow: [StateContainer.of(context).curTheme.boxShadowButton!],
+                                ),
+                                height: 55,
+                                width: (UIUtil.getDrawerAwareScreenWidth(context) - 42).abs() / 2,
+                                margin: const EdgeInsetsDirectional.only(start: 14, top: 0.0, end: 7.0),
+                                // margin: EdgeInsetsDirectional.only(start: 7.0, top: 0.0, end: 7.0),
+                                child: TextButton(
+                                  key: const Key("home_receive_button"),
+                                  style: TextButton.styleFrom(
+                                    backgroundColor:
+                                        receive != null ? StateContainer.of(context).curTheme.primary : StateContainer.of(context).curTheme.primary60,
+                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+                                    primary: receive != null ? StateContainer.of(context).curTheme.background40 : Colors.transparent,
+                                    // highlightColor: receive != null ? StateContainer.of(context).curTheme.background40 : Colors.transparent,
+                                    // splashColor: receive != null ? StateContainer.of(context).curTheme.background40 : Colors.transparent,
+                                  ),
+                                  child: AutoSizeText(
+                                    AppLocalization.of(context)!.receive,
+                                    textAlign: TextAlign.center,
+                                    style: AppStyles.textStyleButtonPrimary(context),
+                                    maxLines: 1,
+                                    stepGranularity: 0.5,
+                                  ),
+                                  onPressed: () {
+                                    if (receive == null) {
+                                      return;
+                                    }
+                                    Sheets.showAppHeightNineSheet(context: context, widget: receive!);
+                                  },
+                                ),
+                              ),
+                              AppPopupButton(),
+                            ],
+                          ),
                         ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ],
