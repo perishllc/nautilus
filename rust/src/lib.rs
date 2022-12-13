08f1e12d134e5c5b91be689fd3e78acfa845b73b
fossephate <fosse@book.local>
2022-12-12 22:08:47 -0500

    fix cargo lipo

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,1 +1,2 @@
 mod username_registration_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
+mod username_registration;
