0ceede5e5159314a8f51dd5812f98303b659c091
fossephate <fosse@book.local>
2022-12-12 21:24:23 -0500

    fix

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,1 @@
-mod bridge_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 mod username_registration_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
-mod username_registration; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
