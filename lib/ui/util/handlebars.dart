2a54f49f2da8664a68fa6b8e2a26abd2d463eac1
fossephate <fosse@book.local>
2022-12-25 22:34:04 -0500

    fix

--- lib/ui/util/handlebars.dart
+++ lib/ui/util/handlebars.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'package:wallet_flutter/appstate_container.dart';
 
 class Handlebars {
-
   static Widget vertical(BuildContext context) {
-    return               Container(
-                width: 4,
-                height: 30,
-                margin: const EdgeInsets.only(right: 22),
-                decoration: BoxDecoration(
-                  color: StateContainer.of(context).curTheme.text45,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              );
+    return Container(
+      width: 4,
+      height: 30,
+      margin: const EdgeInsets.only(right: 22),
+      decoration: BoxDecoration(
+        color: StateContainer.of(context).curTheme.text45,
+        borderRadius: BorderRadius.circular(12),
+      ),
+    );
   }
 }
