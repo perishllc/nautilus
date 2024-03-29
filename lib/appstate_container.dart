c32675dc4215ae2a751ef0fccd5ed70a1daf32a7
fossephate <matt.cfosse@gmail.com>
2024-03-28 17:53:40 -0700

    hotfix

--- lib/appstate_container.dart
+++ lib/appstate_container.dart
@@ -464,7 +464,7 @@ class StateContainerState extends State<StateContainer> {
       if (n2Nodes.isNotEmpty && (wallet?.representative.isNotEmpty ?? false)) {
         final N2Node? node = n2Nodes.firstWhereOrNull((N2Node element) => element.account == wallet!.representative);
         if (node != null) {
-          if (node.score != null && node.score! < 200) {
+          if (node.score != null && node.score! < 90) {
             addActiveOrSettingsAlert(alert, null);
           }
         } else {
